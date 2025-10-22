f03e6195f94fa8e60404f2a2530c035df146123b
Raunak <raunakupadhyay527@gmail.com>
2025-10-22 20:53:08 +0530

    update

--- src/main/java/com/raunak/app/AppApplication.java
+++ src/main/java/com/raunak/app/AppApplication.java
@@ -2,14 +2,16 @@ package com.raunak.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-		dev obj=new dev();
-		obj.build();
+		ApplicationContext context = SpringApplication.run(AppApplication.class, args);
+		dev obj = context.getBean(dev.class);
+		obj.build();	
+
 
 
 	}
--- src/main/java/com/raunak/app/dev.java
+++ src/main/java/com/raunak/app/dev.java
@@ -1,8 +1,11 @@
 package com.raunak.app;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class dev {
     public void build(){
-        System.out.println("working on project");
+        System.out.println("working on awesome project");
 
     }
     
