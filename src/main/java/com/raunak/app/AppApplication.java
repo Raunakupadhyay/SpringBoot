89a23cf28ad1dcd053b4cef599c7efb18ff45863
Raunak <raunakupadhyay527@gmail.com>
2025-10-22 20:08:02 +0530

    update

--- src/main/java/com/raunak/app/AppApplication.java
+++ src/main/java/com/raunak/app/AppApplication.java
@@ -8,6 +8,10 @@ public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+		dev obj=new dev();
+		obj.build();
+
+
 	}
 
 }
---
+++ src/main/java/com/raunak/app/dev.java
@@ -0,0 +1,9 @@
+package com.raunak.app;
+
+public class dev {
+    public void build(){
+        System.out.println("working on project");
+
+    }
+    
+}
