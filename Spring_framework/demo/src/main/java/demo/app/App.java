4926b6d3b5b990a3ec3ef9fca7fd9c2a1176c85c
Raunak <raunakupadhyay527@gmail.com>
2025-11-22 00:16:56 +0530

    Spring framework

--- Spring_framework/demo/src/main/java/demo/app/App.java
+++ Spring_framework/demo/src/main/java/demo/app/App.java
@@ -1,9 +1,12 @@
 package demo.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     }
 }
